dc196174f46e1eeaa9bc34cfcfee152b5071c0e1
AntoCreed777 <abenavides@desem.cl>
2023-04-27 19:56:10 -0400

    Arregle el texto de salida

--- LaboratorioProgramaciónBasica/Ejercicio 5.py
+++ LaboratorioProgramaciónBasica/Ejercicio 5.py
@@ -1,7 +1,7 @@
-n=int(input("Numero de datos que va a ingresar :"))
+n=int(input("Numero de datos que va a ingresar: "))
 while n<=0:
     print("Error, valor mal ingresado")
-    n=int(input("Numero de datos que va a ingresar :"))
+    n=int(input("Numero de datos que va a ingresar: "))
 
 s=0
 m=1
@@ -15,4 +15,4 @@ for i in range(n):
 s=str(s)
 m=str(m)
 
-print("La suma de los numeros ingresados es "+s +" y la multiplicacion de los numeros ingresados es "+m )
+print("La suma de los numeros pares ingresados es "+s +" y la multiplicacion de los numeros impares ingresados es "+m )
