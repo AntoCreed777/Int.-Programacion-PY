b5f5a31737dfc0972ffda6844ba192b2d20f34f0
AntoCreed777 <abenavides@desem.cl>
2023-05-03 08:56:09 -0400

    Mejorado

--- Laboratorio 2/Hitos geologicos.py
+++ Laboratorio 2/Hitos geologicos.py
@@ -1,24 +1,29 @@
-a=input("Ingrese la coordenada X del primer hito: ")
-b=input("Ingrese la coordenada Y del primer hito: ")
-c=input("Ingrese la coordenada Z del primer hito: ")
-i=input("Ingrese la coordenada X del segundo hito: ")
-j=input("Ingrese la coordenada X del segundo hito: ")
-k=input("Ingrese la coordenada X del segundo hito: ")
+a=float(input("Ingrese la coordenada X del primer hito: "))
+b=float(input("Ingrese la coordenada Y del primer hito: "))
+c=float(input("Ingrese la coordenada Z del primer hito: "))
+i=float(input("Ingrese la coordenada X del segundo hito: "))
+j=float(input("Ingrese la coordenada Y del segundo hito: "))
+k=float(input("Ingrese la coordenada Z del segundo hito: "))
 
 Distancia1=(((i-a)**2)+((j-b)**2)+((k-c)**2))**0.5
 
-a=input("Ingrese la coordenada X del primer hito: ")
-b=input("Ingrese la coordenada Y del primer hito: ")
-c=input("Ingrese la coordenada Z del primer hito: ")
-i=input("Ingrese la coordenada X del segundo hito: ")
-j=input("Ingrese la coordenada X del segundo hito: ")
-k=input("Ingrese la coordenada X del segundo hito: ")
+a=float(input("Ingrese la coordenada X del primer hito: "))
+b=float(input("Ingrese la coordenada Y del primer hito: "))
+c=float(input("Ingrese la coordenada Z del primer hito: "))
+i=float(input("Ingrese la coordenada X del segundo hito: "))
+j=float(input("Ingrese la coordenada Y del segundo hito: "))
+k=float(input("Ingrese la coordenada Z del segundo hito: "))
 
 Distancia2=(((i-a)**2)+((j-b)**2)+((k-c)**2))**0.5  
 
 
 Magnitud=((Distancia1 - Distancia2)**2)**0.5
 
-print(Distancia1)
-print(Distancia2)
-print(Magnitud)
+Distancia1=str(Distancia1)
+Distancia2=str(Distancia2)
+Magnitud=str(Magnitud)
+
+
+print("La distancia entre el primer hito y el segundo en el primer momento es de "+Distancia1)
+print("La distancia entre el primer hito y el segundo en el segundo momento es de "+Distancia2)
+print("El desplazamiento total es de "+Magnitud)
