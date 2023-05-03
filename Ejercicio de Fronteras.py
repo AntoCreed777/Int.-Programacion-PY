627ca114082d59ff1a696451161acc6ab1b1e43f
AntoCreed777 <abenavides@desem.cl>
2023-05-03 15:04:27 -0400

    Mejor que antes

--- Ejercicio de Fronteras.py
+++ Ejercicio de Fronteras.py
@@ -4,33 +4,36 @@ while k<=0 or k>1000:
     k=int(input("Numero de consultas va a ingresar: "))
 
 n=int(input("Coordenada X: "))
-while n<=0 or n>1000:
+while n<-1000 or n>1000:
     print("Error, valor mal ingresado")
     n=int(input("Coordenada X: "))
 
 m=int(input("Coordenada Y: "))
-while m<=0 or m>1000:
+while m<-1000 or m>1000:
     print("Error, valor mal ingresado")
     m=int(input("Coordenada Y: "))
 
 while k!=0:
     x=int(input("Coordenada X: "))
-    while x<=0 or x>1000:
+    while x<-1000 or x>1000:
         print("Error, valor mal ingresado")
         x=int(input("Coordenada X: "))
     
     y=int(input("Coordenada Y: "))
-    while y<=0 or y>1000:
+    while y<-1000 or y>1000:
         print("Error, valor mal ingresado")
         y=int(input("Coordenada Y: "))
     if x==n or y==m:
         print("Esta en la Frontera")
-    if x>n:
-        if y>m:
-            print("Nlogonia Noroccidental")
-        print("Nlogonia Sudoriental")
-    if y>m:
-        print("Nlogonia Nororiental")
-    else :
-        print("Nlogonia Sudoccidental")
+    else:
+        if x>n:
+            if y>m:
+                print("Nlogonia Nororiental")
+            else:
+                print("Nlogonia Sudoriental")
+        else:
+            if y>m:
+                print("Nlogonia Noroccidental")
+            else :
+                print("Nlogonia Sudoccidental")
     k=k-1
