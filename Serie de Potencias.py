0a6a013673842c04a4bbc9b783d97dee0188611b
AntoCreed777 <abenavides@desem.cl>
2023-05-26 13:54:12 -0400

    Contador de tiempo

--- Serie de Potencias.py
+++ Serie de Potencias.py
@@ -1,3 +1,4 @@
+import time
 def multiplicacion(x,e):
     aux=0
     for i in range(e):
@@ -20,4 +21,7 @@ while a<0:
         e=int(input("Introdusca la potencia: "))
     if x==0 and e==0:
         break
+    start=time.perf_counter()
     print(f"{x} elevado a {e} es igual a {potencia(x,e)}")
+    end=time.perf_counter()
+    print("IntegralCoseno(con numba) = {}s".format((end - start)))
