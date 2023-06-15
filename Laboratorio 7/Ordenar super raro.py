6cb5d1b92ae691ff67336e33d76096c80fa3f3b9
AntoCreed777 <abenavides@desem.cl>
2023-06-14 22:53:16 -0400

    El de ordenar en la diagonal principal pero bien hecho

--- Laboratorio 7/Ordenar super raro.py
+++ Laboratorio 7/Ordenar super raro.py
@@ -1,11 +1,13 @@
 import numpy as np
+import random
 N=int(input("Tamaño del array: "))
 while N>10 or N<=0:
-    N=int(input("Tamaño del array: "))
+    N=int(input("Tamaño del array: "))  
 vector=np.empty((N,N))
 for i in range(N):
     for j in range(N):
-        vector[i][j]=int(input(f"Ingrese el numero en la fila {i+1} y en la columna {j+1}: "))
+        #vector[i][j]=int(input(f"Ingrese el numero en la fila {i+1} y en la columna {j+1}: "))
+        vector[i][j]=random.randint(1,100)
         #vector[i][j]=i+j
 print(vector)
 for i in range(N):
@@ -15,7 +17,7 @@ for i in range(N):
         posicionant=posicionf-(j+1)
         if posicionant==-1:
             vector[i,i]=mayor
-        else:
+        elif posicionant>0:
             vector[posicionf-j,i]=vector[posicionant,i]
 for i in range(N):
     for j in range(N):
