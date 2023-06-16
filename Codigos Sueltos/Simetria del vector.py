323f4a80fc9264bf22414f794c676fb1e2577e54
AntoCreed777 <abenavides@desem.cl>
2023-06-16 14:13:53 -0400

    Ahora la matriz es de enteros con dtype=int

--- Codigos Sueltos/Simetria del vector.py
+++ Codigos Sueltos/Simetria del vector.py
@@ -10,11 +10,11 @@ def simetria(vector):
 N=int(input("Tamaño del array: "))
 while N>10 or N<=0:
     N=int(input("Tamaño del array: "))  
-vector=np.empty((N,N))
+vector=np.empty((N,N),dtype=int)
 for i in range(N):
     for j in range(N):
-        vector[i][j]=int(input(f"Ingrese el numero en la fila {i+1} y en la columna {j+1}: "))
-        #vector[i][j]=random.randint(1,100)
+        #vector[i][j]=int(input(f"Ingrese el numero en la fila {i+1} y en la columna {j+1}: "))
+        vector[i][j]=random.randint(1,100)
         #vector[i][j]=i+j
 print(vector)
 print(simetria(vector))
